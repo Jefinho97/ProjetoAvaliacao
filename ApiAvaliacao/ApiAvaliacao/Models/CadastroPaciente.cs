03ae63ab2d44f74361c8ba57594bed85177692fc
Jefinho97 <42986914+Jefinho97@users.noreply.github.com>
2019-01-17 01:29:22 -0300

    Cadastro do Paciente

---
+++ ApiAvaliacao/ApiAvaliacao/Models/CadastroPaciente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAvaliacao.Models
+{
+    public class CadastroPaciente
+    {
+        private string Nome { get; set; }
+        private string CPF { get; set; }
+        private DateTime Nascimento { get; set; }
+        private string CEP { get; set; }
+        private string Rua { get; set; }
+        private string NumCasa { get; set; }
+        private string Bairro { get; set; }
+        private string UF { get; set; }
+        private string Telefone { get; set; }
+        private string EstCivil { get; set; }
+        private string Sexo { get; set; }
+        private string Escolaridade { get; set; }
+        private string Profissao { get; set; }
+        private string OpcaoProf { get; set; }
+        private string Raca { get; set; }
+        private string Diagnostico { get; set; }
+    }
+}
